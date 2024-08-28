48b4e61fba533ff755a8fc4de648b2869002fb9a
George Yashin <georgtree@gmail.com>
2024-08-28 20:02:08 +0100

    add multiplot capabilities, slightly changed interface: -delete to -nodelete, documentation update, and temporary files handling update

--- src/gnuplotutil.tcl
+++ src/gnuplotutil.tcl
@@ -5,16 +5,16 @@ package provide gnuplotutil 0.1
 
 namespace eval ::gnuplotutil {
 
-    namespace export plotXYN plotXNYN
+    namespace export plotXYN plotXNYN multiplotXNYN
 }
 
 
 proc gnuplotutil::plotXYN {x args} {
-    # Creates input and command files to plot 2D graphs with the same x points, and send it to gnuplot.
+    # Plots 2D graphs in Gnuplot with the same x-values.
     #  x - List that contains x-point for 2D graph
     #  -xlog - arg xlog boolean switch of log scale of x axis, default off
     #  -ylog - boolean switch of log scale of y axis, default off
-    #  -delete - boolean switch that enables deleting of temporary file after end of plotting, default off
+    #  -nodelete - boolean switch that disables deleting of temporary file after end of plotting, default off
     #  -xlabel - argument to set x-axis label to display, string must be provided after it
     #  -ylabel - argument to set y-axis label to display, string must be provided after it
     #  -optcmd - argument with optional string that may contain additional commands to gnuplot
@@ -23,7 +23,7 @@ proc gnuplotutil::plotXYN {x args} {
     #    in the same order as data columns provided, must have the length 1+number of y data colums
     #  -columns - argument that provides the y data to plot, the number of columns is not restricted and must be provided
     #    at the end of command after all switches
-    # Returns gnuplot window with plotted data
+    # Returns: gnuplot window with plotted data
     # ```
     # Example:
     # set x [list 0 1 2 3 4 5 6]
@@ -34,7 +34,7 @@ proc gnuplotutil::plotXYN {x args} {
     set arguments [argparse -inline {
         {-xlog -boolean}
         {-ylog -boolean}
-        {-delete -boolean}
+        {-nodelete -boolean}
         {-xlabel -argument}
         {-ylabel -argument}
         {-optcmd -argument}
@@ -73,7 +73,7 @@ proc gnuplotutil::plotXYN {x args} {
     set yColumnCount 0
     foreach val [dict get $arguments columns] {
         if {[llength $val] != [llength $x]} {
-            puts stderr "Number of points of y-axis data (column $yColumnCount) doesn't match the number of points of x-axis data"
+            error "Number of points of y-axis data (column $yColumnCount) doesn't match the number of points of x-axis data"
             incr yColumnCount
         }
     }
@@ -81,7 +81,7 @@ proc gnuplotutil::plotXYN {x args} {
     set numCol [llength [dict get $arguments columns]]
     if {([dict exist $arguments names]==1)} {
         if {[llength $columnNames] != [expr {$numCol}]} {
-            puts stderr "Column names count is not the same as count of data columns"
+            error "Column names count is not the same as count of data columns"
             set autoTitleStr ""
         } else {
             lappend outList "{ } $columnNames"
@@ -97,16 +97,23 @@ proc gnuplotutil::plotXYN {x args} {
         lappend outList $row
     }
     # save data to temporary file
-    set randId [expr {int(rand()*10000)}]
-    set resFile [open tmpPlotId${randId}.csv w+]
+    set counter 0
+    while {true} {
+        if {[file exists gnuplotTemp${counter}.csv]} {
+           incr counter
+        } else {
+            set resFile [open gnuplotTemp${counter}.csv w+]
+            break
+        }
+    }
     puts $resFile [::csv::joinlist $outList " "]
     close $resFile
     # create command strings for gnuplot
-    set commandStr "plot 'tmpPlotId${randId}.csv' using 1:2 with lines "
+    set commandStr "plot 'gnuplotTemp${counter}.csv' using 1:2 with lines "
     for {set i 1} {$i<$numCol} {incr i} {
         set commandStr  "${commandStr}, '' using 1:[expr {$i+2}] with lines "
     }
-    exec gnuplot << "
+    catch {exec gnuplot << "
         set mouse
         $optcmdStr
         $autoTitleStr
@@ -117,17 +124,19 @@ proc gnuplotutil::plotXYN {x args} {
         $gridStr
         $commandStr
         pause mouse close
-    "
-    if {[dict get $arguments delete]==1} {
-        file delete tmpPlotId${randId}.csv
+    "} errorStr
+    puts $errorStr
+    if {[dict get $arguments nodelete]==0} {
+        file delete gnuplotTemp${counter}.csv
     }
 }
 
 proc gnuplotutil::plotXNYN {args} {
-    # Creates input and command files to plot 2D graphs with the individual x points, and send it to gnuplot.
+    # Plots 2D graphs in Gnuplot with individual x-values.
+    # Creates input and command files to plot 2D graphs with the individual x points, and sends it to gnuplot.
     #  -xlog - arg xlog boolean switch of log scale of x axis, default off
     #  -ylog - boolean switch of log scale of y axis, default off
-    #  -delete - boolean switch that enables deleting of temporary file after end of plotting, default off
+    #  -nodelete - boolean switch that disables deleting of temporary file after end of plotting, default off
     #  -xlabel - argument to set x-axis label to display, string must be provided after it
     #  -ylabel - argument to set y-axis label to display, string must be provided after it
     #  -optcmd - argument with optional string that may contain additional commands to gnuplot
@@ -136,7 +145,7 @@ proc gnuplotutil::plotXNYN {args} {
     #    in the same order as data columns provided, must have the length 1+number of y data colums
     #  -columns -  argument that provides the x and y data to plot, the number of columns is not restricted and must be provided
     #    at the end of command after all switches
-    # Returns gnuplot window with plotted data
+    # Returns: gnuplot window with plotted data
     # ```
     # Example:
     # set x1 [list 0 1 2 3 4 5 6]
@@ -152,7 +161,7 @@ proc gnuplotutil::plotXNYN {args} {
     set arguments [argparse -inline {
         {-xlog -boolean}
         {-ylog -boolean}
-        {-delete -boolean}
+        {-nodelete -boolean}
         {-xlabel -argument}
         {-ylabel -argument}
         {-optcmd -argument}
@@ -207,7 +216,7 @@ proc gnuplotutil::plotXNYN {args} {
     if {([dict exist $arguments names]==1)} {
             set headerString {}
         if {[llength $columnNames] != [expr {$dataNum}]} {
-            puts stderr "Column names count is not the same as count of data columns"
+            error "Column names count is not the same as count of data columns"
             set autoTitleStr ""
         } else {
             for {set i 0} {$i<=$dataNum} {incr i} {
@@ -233,16 +242,23 @@ proc gnuplotutil::plotXNYN {args} {
         lappend outList $row
     }
     # save data to temporary file
-    set randId [expr {int(rand()*10000)}]
-    set resFile [open tmpPlotId${randId}.csv w+]
+    set counter 0
+    while {true} {
+        if {[file exists gnuplotTemp${counter}.csv]} {
+           incr counter
+        } else {
+            set resFile [open gnuplotTemp${counter}.csv w+]
+            break
+        }
+    }
     puts $resFile [::csv::joinlist $outList " "]
     close $resFile
     # create command strings for gnuplot
-    set commandStr "plot 'tmpPlotId${randId}.csv' using 1:2 with lines "
+    set commandStr "plot 'gnuplotTemp${counter}.csv' using 1:2 with lines "
     for {set i 1} {$i<$dataNum} {incr i} {
         set commandStr  "${commandStr}, '' using [expr {$i*2+1}]:[expr {$i*2+2}] with lines "
     }
-    exec gnuplot << "
+    catch {exec gnuplot << "
         set mouse
         $optcmdStr
         $autoTitleStr
@@ -253,8 +269,204 @@ proc gnuplotutil::plotXNYN {args} {
         $gridStr
         $commandStr
         pause mouse close
-    "
-    if {[dict get $arguments delete]==1} {
-        file delete tmpPlotId${randId}.csv
+    "} errorStr
+    puts $errorStr
+    if {[dict get $arguments nodelete]==0} {
+        file delete gnuplotTemp${counter}.csv
     }
+}
+
+
+proc gnuplotutil::plotXNYNMp {args} {
+    # Auxilary function for gnuplotutil::multiplotXNYN, creates command strings and data files
+    #  for individual plots
+    #  -xlog - arg xlog boolean switch of log scale of x axis, default off
+    #  -ylog - boolean switch of log scale of y axis, default off
+    #  -xlabel - argument to set x-axis label to display, string must be provided after it
+    #  -ylabel - argument to set y-axis label to display, string must be provided after it
+    #  -grid - boolean switch that enables display of grid, default off
+    #  -names - argument that enable setting the column names of provided data, value must be provided as list
+    #    in the same order as data columns provided, must have the length 1+number of y data colums
+    #  -columns -  argument that provides the x and y data to plot, the number of columns is not restricted and must be provided
+    #    at the end of command after all switches
+    # Returns: list that contains command script and name of data file 
+    set arguments [argparse -inline {
+        {-xlog -boolean}
+        {-ylog -boolean}
+        {-xlabel -argument}
+        {-ylabel -argument}
+        {-grid -boolean}
+        {-names -argument}
+        {-columns -catchall}
+    }]
+    set xscaleStr ""
+    set yscaleStr ""
+    set xlabelStr ""
+    set ylabelStr ""
+    set gridStr ""
+    set xscaleStrUnset ""
+    set yscaleStrUnset ""
+    set xlabelStrUnset ""
+    set ylabelStrUnset ""
+    set gridStrUnset ""
+    set autoTitleStr ""
+    set optcmdStr ""
+    if {[dict get $arguments xlog]==1} {
+        set xscaleStr "set logscale x"
+        set xscaleStrUnset "unset logscale x"
+    }
+    if {[dict get $arguments ylog]==1} {
+        set yscaleStr "set logscale y"
+        set yscaleStrUnset "unset logscale y"
+    }
+    if {[dict get $arguments grid]==1} {
+        set gridStr "set grid"
+        set gridStrUnset "unset grid"
+    }
+    if {[dict exist $arguments names]==1} {
+        set columnNames [dict get $arguments names]
+    }
+    if {[dict exist $arguments xlabel]==1} {
+        set xlabelStr "set xlabel '[dict get $arguments xlabel]'"
+        set xlabelStrUnset "unset xlabel"
+    }
+    if {[dict exist $arguments ylabel]==1} {
+        set ylabelStr "set ylabel '[dict get $arguments ylabel]'"
+        set ylabelStrUnset "unset ylabel"
+    }
+    set columnsNum [llength [dict get $arguments columns]]
+    if {$columnsNum % 2 != 0} {
+        error "Number of data columns $columnsNum is odd"
+    }
+    set dataNum [expr {int($columnsNum/2)}]
+    set yColumnCount 0
+    for {set i 0} {$i<$dataNum} {incr i} {
+        set xLen [llength [lindex [dict get $arguments columns] [expr {int($i*2)}]]]
+        set yLen [llength [lindex [dict get $arguments columns] [expr {int($i*2+1)}]]]
+        lappend xLengths $xLen
+        if {$xLen != $yLen} {
+            error "Number of points of y-axis data (column [expr {1+$yColumnCount}]) doesn't match the number of points of x-axis data (column $yColumnCount)"
+        }
+        incr yColumnCount
+    }
+    # fill output structure with values
+    if {([dict exist $arguments names]==1)} {
+            set headerString {}
+        if {[llength $columnNames] != [expr {$dataNum}]} {
+            error "Column names count is not the same as count of data columns"
+            set autoTitleStr ""
+        } else {
+            for {set i 0} {$i<=$dataNum} {incr i} {
+                set headerString "${headerString} { } [lindex $columnNames $i]"
+            }
+            lappend outList $headerString
+            set autoTitleStr "set key autotitle columnheader"
+        }
+    }
+    set numRow [tcl::mathfunc::max {*}$xLengths]
+    for {set i 0} {$i<$numRow} {incr i} {
+        set row {}
+        for {set j 0} {$j<$dataNum} {incr j} {
+            set columns [dict get $arguments columns]
+            set xVal [lindex [lindex $columns [expr {int($j*2)}]] $i]
+            set yVal [lindex [lindex $columns [expr {int($j*2+1)}]] $i]
+            if {$xVal=={}} {
+                lappend row { } { }
+            } else {
+                lappend row $xVal $yVal
+            }
+        }
+        lappend outList $row
+    }
+    # save data to temporary file
+    set counter 0
+    while {true} {
+        if {[file exists gnuplotTemp${counter}.csv]} {
+           incr counter
+        } else {
+            set resFile [open gnuplotTemp${counter}.csv w+]
+            break
+        }
+    }
+    puts $resFile [::csv::joinlist $outList " "]
+    close $resFile
+    # create command strings for gnuplot
+    set commandStr "plot 'gnuplotTemp${counter}.csv' using 1:2 with lines "
+    for {set i 1} {$i<$dataNum} {incr i} {
+        set commandStr  "${commandStr}, '' using [expr {$i*2+1}]:[expr {$i*2+2}] with lines "
+    }
+    set gnuplotCmdString "
+        $autoTitleStr
+        $xlabelStr
+        $ylabelStr
+        $xscaleStr
+        $yscaleStr
+        $gridStr
+        $commandStr
+        $xlabelStrUnset
+        $ylabelStrUnset
+        $xscaleStrUnset
+        $yscaleStrUnset
+        $gridStrUnset
+    "
+    return [dict create cmdString $gnuplotCmdString file "gnuplotTemp${counter}.csv"]
+}
+
+
+
+proc gnuplotutil::multiplotXNYN {layout args} {
+    # Plots 2D graphs in Gnuplot with individual x-values and by using multiplot.
+    #  layout - list of layout configurations values, for example, {2 2}
+    #  -nodelete - boolean switch that disables deleting of temporary file after end of plotting, default off
+    #  -optcmd - argument with optional string that may contain additional commands to gnuplot
+    #  -plots - argument that provides the list of individual plots, the number of plots is not restricted and must be provided
+    #    at the end of command after all switches, the inputs syntax is the same as [::gnuplotutil::plotXNYN]
+    # Returns: gnuplot window with plotted data as multiplot
+    # ```
+    # Example:
+    # set x1 [list 1 2 3 4 5 6]
+    # set y1 [list 1 4 9 16 25 36]
+    # set x2 [list 6 5 4 3 2 1]
+    # set y2 [list 36 25 16 9 4 1]
+    # set x3 [list 7 8 9 10]
+    # set y3 [list 49 64 81 100]
+    # set plot1 [list -xlog -ylog -xlabel "x label" -ylabel "y label" -grid -names [list name1 name2 name3] -columns $x1 $y1 $x2 $y2 $x3 $y3]
+    # set plot2 [list -ylabel "y label" -grid -names [list name1 name2] -columns $x1 $y1 $x2 $y2 ]
+    # set plot3 [list -xlabel "x label" -ylabel "y label" -names [list name3] -columns $x3 $y3 ]
+    # set plot4 [list -grid -names [list name2] -columns $x2 $y2 ]
+    # gnuplotutil::multiplotXNYN {2 2} -plots $plot1 $plot2 $plot3 $plot4
+    # ```
+    set arguments [argparse {
+        {-optcmd -argument}
+        {-nodelete -boolean}
+        {-plots -catchall}
+    }]
+    if {[info exists optcmd]==0} {
+        set optcmd ""
+    }
+    if {[llength $layout]>2} {
+        error "Length of layout arguments is more than 2"
+    } else {
+        set layoutStr "set multiplot layout [lindex $layout 0],[lindex $layout 1]"
+    }
+
+    foreach plot $plots {
+        set plotResults [gnuplotutil::plotXNYNMp {*}$plot]
+        lappend cmdStrings [dict get $plotResults cmdString]
+        lappend fileNames [dict get $plotResults file]
+    }
+    catch {exec gnuplot << "
+        $optcmd
+        $layoutStr
+        [join $cmdStrings \n]
+        unset multiplot
+        pause mouse close
+    "} errorStr
+    puts $errorStr
+    if {$nodelete==0} {
+        foreach fileName $fileNames {
+            file delete $fileName
+        }
+    }
+
 }
---
+++ test/arbitaryTests.tcl
@@ -0,0 +1,18 @@
+set script_path [file dirname [file normalize [info script]]]
+lappend auto_path "$script_path/../"
+package require gnuplotutil
+
+
+set x1 [list 1 2 3 4 5 6]
+set y1 [list 1 4 9 16 25 36]
+set x2 [list 6 5 4 3 2 1]
+set y2 [list 36 25 16 9 4 1]
+set x3 [list 7 8 9 10]
+set y3 [list 49 64 81 100]
+
+        
+set plot1 [list -xlog -ylog -xlabel "x label" -ylabel "y label" -grid -names [list name1 name2 name3] -columns $x1 $y1 $x2 $y2 $x3 $y3]
+set plot2 [list -ylabel "y label" -grid -names [list name1 name2] -columns $x1 $y1 $x2 $y2 ]
+set plot3 [list -xlabel "x label" -ylabel "y label" -names [list name3] -columns $x3 $y3 ]
+set plot4 [list -grid -names [list name2] -columns $x2 $y2 ]
+gnuplotutil::multiplotXNYN {2 2} -plots $plot1 $plot2 $plot3 $plot4
