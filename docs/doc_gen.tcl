d606eb40465f3c7824f3d28cb79793c46db3fd02
georgtree <georgtree@gmail.com>
2024-12-24 12:53:37 +0000

    docs update

--- docs/doc_gen.tcl
+++ docs/doc_gen.tcl
@@ -3,9 +3,9 @@ set path_to_hl_tcl "/home/georgtree/tcl/hl_tcl"
 package require ruff
 package require fileutil
 package require hl_tcl
-set dir [file dirname [file normalize [info script]]]
-set sourceDir "${dir}/.."
-source [file join $dir startPage.ruff]
+set docDir [file dirname [file normalize [info script]]]
+set sourceDir "${docDir}/.."
+source [file join $docDir startPage.ruff]
 source [file join $sourceDir gnuplotutil.tcl]
 source [file join $sourceDir mathutil.tcl]
 source [file join $sourceDir rfutil.tcl]
@@ -24,16 +24,16 @@ set commonNroff [list -title $title -sortnamespaces false -preamble $startPage -
 set namespaces [list ::gnuplotutil ::mathutil ::rfutil ::touchstoneutil]
 
 if {[llength $argv] == 0 || "html" in $argv} {
-    ruff::document $namespaces -format html -outdir $dir -outfile index.html {*}$commonHtml
-    ruff::document $namespaces -format nroff -outdir $dir -outfile tcl_tools.n {*}$commonNroff
+    ruff::document $namespaces -format html -outdir $docDir -outfile index.html {*}$commonHtml
+    ruff::document $namespaces -format nroff -outdir $docDir -outfile tcl_tools.n {*}$commonNroff
 }
 
-foreach file [glob *.html] {
-    exec tclsh "${path_to_hl_tcl}/tcl_html.tcl" "./$file"
+foreach file [glob ${docDir}/*.html] {
+    exec tclsh "${path_to_hl_tcl}/tcl_html.tcl" [file join ${docDir} $file]
 }
 
 proc processContents {fileContents} {
     return [string map {max-width:60rem max-width:100rem} $fileContents]
 }
 
-fileutil::updateInPlace [file join $dir assets ruff-min.css] processContents
+fileutil::updateInPlace [file join $docDir assets ruff-min.css] processContents
